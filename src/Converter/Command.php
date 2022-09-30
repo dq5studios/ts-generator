1d11aaeff2e70bd5d1ad7744b7258a47fa40ceb8
Ben Dusinberre <ben@dq5studios.com>
2022-09-29 21:26:37 -0500

    Flesh out the command

--- src/Converter/Command.php
+++ src/Converter/Command.php
@@ -8,6 +8,7 @@ use Composer\InstalledVersions;
 use DQ5Studios\TypeScript\Generator\File;
 use DQ5Studios\TypeScript\Generator\Printer;
 use Roave\BetterReflection\BetterReflection;
+use Roave\BetterReflection\Reflection\Adapter\ReflectionClass;
 use Roave\BetterReflection\Reflector\DefaultReflector;
 use Roave\BetterReflection\SourceLocator\SourceStubber\ReflectionSourceStubber;
 use Roave\BetterReflection\SourceLocator\Type\AggregateSourceLocator;
@@ -17,6 +18,7 @@ use Roave\BetterReflection\SourceLocator\Type\DirectoriesSourceLocator;
 use Roave\BetterReflection\SourceLocator\Type\PhpInternalSourceLocator;
 use Roave\BetterReflection\SourceLocator\Type\SingleFileSourceLocator;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
@@ -29,13 +31,21 @@ class Command extends SymfonyCommand
 {
     protected static $defaultName = "convert";
 
+    public const INPUT_PROJECT = 0;
+    public const INPUT_FILE = 1;
+    public const INPUT_DIR = 2;
+    public const OUTPUT_FILE = 0;
+    public const OUTPUT_DIR = 1;
+
     protected function configure(): void
     {
+        $this->setName("convert");
         $this->setDescription("Convert PHP files to TypeScript definition files");
         $this->setDefinition(
             new InputDefinition([
                 new InputOption("input", "i", InputOption::VALUE_OPTIONAL, "Input directory or file"),
                 new InputOption("output", "o", InputOption::VALUE_OPTIONAL, "Output directory or file"),
+                new InputOption("overwrite", null, InputOption::VALUE_NONE, "Overwrite existing output file(s)"),
                 new InputArgument("input", InputArgument::OPTIONAL, "Input directory or file"),
             ])
         );
@@ -43,50 +53,69 @@ class Command extends SymfonyCommand
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $io = new SymfonyStyle($input, $output);
+
         $input_loc = (string) ($input->getOption("input") ?? $input->getArgument("input"));
         $output_loc = (string) $input->getOption("output");
 
-        $io = new SymfonyStyle($input, $output);
-        $io->note("Looking in {$input_loc}; writing to {$output_loc}");
-
-        if (!is_file($input_loc)) {
-            $io->error("Only file input currently supported");
-
+        $input_mode = null;
+        if (empty($input_loc)) {
+            $input_mode = Command::INPUT_PROJECT;
+        } else {
+            if (is_dir($input_loc)) {
+                $input_mode = Command::INPUT_DIR;
+            } elseif (is_file($input_loc)) {
+                $input_mode = Command::INPUT_FILE;
+            }
+        }
+        if (is_null($input_mode)) {
+            $io->error("Input directory/file not found");
             return Command::FAILURE;
         }
 
-        // $classLoader = require __DIR__ . "/../vendor/autoload.php";
-
-        // $astLocator = (new BetterReflection())->astLocator();
-        // $reflector = new DefaultReflector(new ComposerSourceLocator($classLoader, $astLocator));
-
-        // Load composer
-        $astLocator = (new BetterReflection())->astLocator();
-        $reflector = new DefaultReflector(new AggregateSourceLocator([
-            (new MakeLocatorForComposerJson())(InstalledVersions::getRootPackage()["install_path"], $astLocator),
-            new PhpInternalSourceLocator($astLocator, new ReflectionSourceStubber()),
-        ]));
-        $classes = $reflector->reflectAllClasses();
-        foreach ($classes as $class) {
-            echo $class->getName(), PHP_EOL;
+        $output_mode = null;
+        if (is_dir($output_loc)) {
+            $output_mode = Command::OUTPUT_DIR;
+        } elseif (is_file($output_loc)) {
+            $output_mode = Command::OUTPUT_FILE;
+            $io->confirm("Output file already exists, overwrite?", true);
+        }
+        // TODO: If doesn't exist yet, figure out if file/dir
+        // TODO: If not set, put files along side input
+        if (is_null($output_mode)) {
+            $io->info("What to do with the output");
         }
 
-        // Single File
-        $astLocator = (new BetterReflection())->astLocator();
-        $reflector = new DefaultReflector(new SingleFileSourceLocator($input_loc, $astLocator));
+        $io->info("Looking in {$input_loc}; writing to {$output_loc}");
 
-        // Single directory
-        // $directoriesSourceLocator = new DirectoriesSourceLocator(['path/to/directory1'], $astLocator);
-        // $reflector = new DefaultReflector($directoriesSourceLocator);
+        $ast_locator = (new BetterReflection())->astLocator();
+
+        /** @psalm-suppress InternalClass, InternalMethod */
+        $reflector = match ($input_mode) {
+            Command::INPUT_PROJECT => new DefaultReflector(
+                new AggregateSourceLocator([
+                    (new MakeLocatorForComposerJson())(InstalledVersions::getRootPackage()["install_path"], $ast_locator),
+                    new PhpInternalSourceLocator($ast_locator, new ReflectionSourceStubber()),
+                ])
+            ),
+            Command::INPUT_FILE => new DefaultReflector(new SingleFileSourceLocator($input_loc, $ast_locator)),
+            Command::INPUT_DIR => new DefaultReflector(new DirectoriesSourceLocator([$input_loc], $ast_locator)),
+        };
 
         $classes = $reflector->reflectAllClasses();
 
         $file = new File();
-        foreach ($classes as $class) {
-            $io->info("Converting {$class->getShortName()} to {$output_loc}");
-            $file->append(Convert::fromPHP($class->getName()));
+        /** @var ReflectionClass $class */
+        foreach ($io->progressIterate($classes) as $class) {
+            if (!$class->isAnonymous()) {
+                // $io->info("Converting {$class->getShortName()}");
+                // $file->append(Convert::fromPHP($class->getName()));
+            }
+            // $progress->advance();
         }
         file_put_contents($output_loc, Printer::print($file));
+
+        $io->success("Finished!");
 
         // $finder = new Finder();
         // $finder->files()->in(__DIR__)->name('/\.php$/');
